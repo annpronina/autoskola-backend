5f85ca5bf616260cb5610050c9ac57dc7ef705c9
annpronina <anna.proninar1ck4@gmail.com>
2026-03-25 22:14:31 +0200

    Add Student model and migration

---
+++ app/Models/Student.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'name',
+        'surname',
+        'personal_code',
+        'address',
+        'phone',
+        'email',
+        'category_id'
+    ];
+}
