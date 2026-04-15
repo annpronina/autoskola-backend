13af186b71f62f39a9b78be9ebb9e49adacf3c39
annpronina <anna.proninar1ck4@gmail.com>
2026-04-15 20:43:21 +0300

    Create VehicleType database seeder

---
+++ database/seeders/VehicleTypeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class VehicleTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('vehicle_types')->insert([
+            ['type' => 'Car'],
+            ['type' => 'Motorcycle'],
+            ['type' => 'Scooter'],
+            ['type' => 'Trailer']
+        ]);
+    }
+}
