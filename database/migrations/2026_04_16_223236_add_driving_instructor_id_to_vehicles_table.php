9b1ec719de411321bcb86874bff9f661312f6f02
annpronina <anna.proninar1ck4@gmail.com>
2026-04-17 17:14:52 +0300

    Move driving_instructor relation to vehicles table

---
+++ database/migrations/2026_04_16_223236_add_driving_instructor_id_to_vehicles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->foreignId('driving_instructor_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropForeign(['driving_instructor_id']);
+            $table->dropColumn('driving_instructor_id');
+        });
+    }
+};
