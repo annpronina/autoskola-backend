28c62b2088e811658904ad1fb2a7e7c6e291fbe6
annpronina <anna.proninar1ck4@gmail.com>
2026-04-19 17:48:06 +0300

    Update admin panel brand name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Autoskolas sistēma')
             ->colors([
                 'primary' => Color::Amber,
             ])
