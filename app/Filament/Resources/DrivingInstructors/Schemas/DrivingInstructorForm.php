41e5191dc65111ffc4235249578fd0f3ca04e9b9
annpronina <anna.proninar1ck4@gmail.com>
2026-04-22 18:03:59 +0300

    Improve DrivingInstructor form inputs and replace DatePicker

--- app/Filament/Resources/DrivingInstructors/Schemas/DrivingInstructorForm.php
+++ app/Filament/Resources/DrivingInstructors/Schemas/DrivingInstructorForm.php
@@ -6,6 +6,7 @@ namespace App\Filament\Resources\DrivingInstructors\Schemas;
 use Filament\Forms\Components\CheckboxList;
 use Filament\Support\Enums\GridDirection;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Schemas\Components\Section;
@@ -23,35 +24,41 @@ class DrivingInstructorForm
                     ->schema([
                         TextInput::make('name')
                             ->label('Vārds')
-                            ->required()
-                            ->maxLength(255),
+                            ->maxLength(255)
+                            ->placeholder('Ievadiet vārdu')
+                            ->required(),
 
                         TextInput::make('surname')
                             ->label('Uzvārds')
-                            ->required()
-                            ->maxLength(255),
+                            ->maxLength(255)
+                            ->placeholder('Ievadiet uzvārdu')
+                            ->required(),
 
                         TextInput::make('personal_code')
                             ->label('Personas kods')
-                            ->required()
-                            ->maxLength(255),
+                            ->maxLength(255)
+                            ->placeholder('Ievadiet personas kodu')
+                            ->required(),
 
                         TextInput::make('email')
                             ->label('E-pasta adrese')
                             ->email()
-                            ->required()
-                            ->maxLength(255),
+                            ->maxLength(255)
+                            ->placeholder('janis.berzins@example.com')
+                            ->required(),
 
                         TextInput::make('phone')
                             ->label('Telefona numurs')
                             ->tel()
-                            ->required()
-                            ->maxLength(255),
+                            ->maxLength(255)
+                            ->placeholder('Ievadiet telefona numuru')
+                            ->required(),
 
                         TextInput::make('address')
                             ->label('Adrese')
-                            ->required()
-                            ->maxLength(255),
+                            ->maxLength(255)
+                            ->placeholder('Ievadiet dzīvesvietas adresi')
+                            ->required(),
                         ])
                         ->collapsible()
                         ->columnSpanFull(),
@@ -60,31 +67,29 @@ class DrivingInstructorForm
                     ->description('Informācija par instruktora darbu un piešķirtajām kategorijām')
                     ->columns(2)
                     ->schema([
-                        TextInput::make('vehicle')
-                            ->label('Transportlīdzeklis')
+                         Select::make('registered_since')
+                            ->label('Braukšanas instruktors kopš')
                             ->required()
-                            ->maxLength(255),
-
-                        DatePicker::make('registered_since')
-                            ->label('Instruktors kopš')
-                            ->required()
-                            ->native(false)
-                            ->displayFormat('d/m/Y')
-                            ->maxDate(now()),
+                            ->options(
+                                collect(range(date('Y'), 2010))
+                                ->mapWithKeys(fn ($year) => [$year => $year])
+                            )
+                            ->placeholder('Izvēlies gadu'),
 
                         Textarea::make('description')
                             ->label('Apraksts')
                             ->rows(4)
                             ->columnSpanFull()
-                            ->maxLength(255),
+                            ->maxLength(255)
+                            ->placeholder('Papildus informācija par braukšanas instruktoru (nav obligāts)...'),
 
                         CheckboxList::make('categories')
                             ->label('Kategorijas')
                             ->relationship(name: 'categories', titleAttribute: 'name')
-                            ->required()
                             ->columns(4)
                             ->columnSpanFull()
                             ->gridDirection(GridDirection::Row)
+                            ->required()
                         ])
                         ->collapsible()
                         ->columnSpanFull()
